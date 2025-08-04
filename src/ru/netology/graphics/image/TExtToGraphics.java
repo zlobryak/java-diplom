5097e71877e9cab9fad928164199aa2d30bc4f28
Zlobryak <clychnik@gmail.com>
2025-08-04 19:54:33 +0300

    textToColor done

---
+++ src/ru/netology/graphics/image/TExtToGraphics.java
@@ -0,0 +1,30 @@
+package ru.netology.graphics.image;
+
+import java.io.IOException;
+
+public class TExtToGraphics implements TextGraphicsConverter{
+    @Override
+    public String convert(String url) throws IOException, BadImageSizeException {
+        return "";
+    }
+
+    @Override
+    public void setMaxWidth(int width) {
+
+    }
+
+    @Override
+    public void setMaxHeight(int height) {
+
+    }
+
+    @Override
+    public void setMaxRatio(double maxRatio) {
+
+    }
+
+    @Override
+    public void setTextColorSchema(TextColorSchema schema) {
+
+    }
+}
--- src/ru/netology/graphics/image/TextColorSchema.java
+++ src/ru/netology/graphics/image/TextColorSchema.java
@@ -3,3 +3,4 @@ package ru.netology.graphics.image;
 public interface TextColorSchema {
     char convert(int color);
 }
+
